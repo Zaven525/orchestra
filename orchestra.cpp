1572b961afa86f4d22b864f0be20776d1eee3507
Zaven525 <71457724+Zaven525@users.noreply.github.com>
2025-11-16 17:48:41 +0400

    Add files via upload

--- orchestra.cpp
+++ orchestra.cpp
@@ -1,26 +1,27 @@
 #include <iostream>
 
+
 class Instrument 
 {
 public:
     virtual ~Instrument() = default;
-    virtual void play() const { std::cout << "Insturment is playing" << std::endl; }
+    virtual void play() const { std::cout << "Instrument is playing" << std::endl; }
 };
 
-class Piano : Instrument
+class Piano : public Instrument
 {
 public:
     virtual void play() const override { std::cout << "Piano is playing" << std::endl; }
 };
 
-class Violin : Instrument
+class Violin : public Instrument
 {
 public:
 
     virtual void play() const override { std::cout << "Violin is playing" << std::endl; }
 };
 
-class Drum : Instrument
+class Drum : public Instrument
 {
 public:
     virtual void play() const override { std::cout << "Drum is playing" << std::endl; }
@@ -29,6 +30,16 @@ public:
 
 int main()
 {
-    
+    Piano p; Violin v; Drum d;
+    Instrument * orchestra[3];
+    orchestra[0] = new Piano;
+    orchestra[1] = new Violin;
+    orchestra[2] = new Drum;
+
+    for (int i = 0; i < 3; i++)
+    {
+        orchestra[i]->play();
+        delete orchestra[i];
+    }
     return 0;
 }
